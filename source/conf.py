41f93bc790bc9f9707b8ff35d5a9740ebca3add9
Karen Brier <kcbrier@sharedgeo.org>
2025-05-20 10:43:45 -0400

    css error

--- source/conf.py
+++ source/conf.py
@@ -29,6 +29,4 @@ html_favicon = "_static/fuzionviewicon.ico"
 import sphinx_rtd_theme
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
-html_css_files = [
-    'css/custom.css',
-    ]
+html_css_files = ['css/custom.css']
