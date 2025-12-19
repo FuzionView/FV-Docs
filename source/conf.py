9ec74a9f3176183ca98b26028f6df1c628c2ed85
Karen Brier <kcbrier@sharedgeo.org>
2025-12-18 20:35:04 -0500

    copyright changes

--- source/conf.py
+++ source/conf.py
@@ -1,18 +1,9 @@
 # Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
-project = 'FuzionView BETA'
-copyright = '2025 SharedGeo'
+project = 'FuzionView'
+copyright = '2025 SharedGeo CC-BY-SA-4.0'
 author = 'SharedGeo'
-release = 'Beta'
-
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+release = '1.0'
 
 extensions = [
 	'sphinx.ext.autosectionlabel',
@@ -25,16 +16,18 @@ templates_path = ['_templates']
 exclude_patterns = []
 
 #html_theme_options = {
-#    `collapse_navigation`: True,
 #    `display_version` : True,
 #    `prev_next_buttons_location` : Both,
 #}
 
+html_theme_options = {
+    'collapse_navigation': True
+}
+
 html_favicon = "_static/fuzionviewicon.ico"
-# -- Options for the HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 import sphinx_rtd_theme
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 html_css_files = ['css/custom.css']
+html_js_files = ['js/custom.js']
